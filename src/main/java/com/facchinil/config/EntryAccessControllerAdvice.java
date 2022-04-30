a6be90e8d149c2e7d0ec050cce5d1ce525400557
Leonardo Facchini <leonardofacchini1997@gmail.com>
2022-04-30 02:15:46 +0200

    code cleaning

--- src/main/java/com/facchinil/config/EntryAccessControllerAdvice.java
+++
@@ -1,9 +0,0 @@
-package com.facchinil.config;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-@ControllerAdvice
-public class EntryAccessControllerAdvice extends ResponseEntityExceptionHandler {
-	
-}
